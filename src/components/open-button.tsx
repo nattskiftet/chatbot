13a30c7f5ddec9b547f8e06106ccff379cdbba1a
thomaslindstr_m <t@hom.as>
2020-11-27 16:14:37 +0100

    Handle more overrides

--- src/components/open-button.tsx
+++ src/components/open-button.tsx
@@ -15,8 +15,6 @@ interface ButtonElementProperties {
 const ButtonElement = styled.button`
     appearance: none;
     background: #fff;
-    margin-right: ${openButtonAvatarSizeNumber / 2}px;
-    margin-bottom: ${openButtonAvatarSizeNumber / 5}px;
     padding: 8px 15px;
     position: fixed;
     bottom: 25px;
@@ -30,18 +28,20 @@ const ButtonElement = styled.button`
             : `scale(0.8) translate3d(0,${
                   openButtonAvatarSizeNumber * 2
               }px,0)`};
-
     opacity: ${(properties: ButtonElementProperties) =>
         properties.isVisible ? '1' : '0'};
-
     transition: ${(properties: ButtonElementProperties) =>
         properties.isVisible
             ? 'transform 0.5s, opacity 0.2s 0.3s'
             : 'transform 0.2s, opacity 0.1s'};
-
     box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.4),
         0 0 0 2px rgba(255, 255, 255, 1), 0 1px 4px rgba(0, 0, 0, 0.5),
         0 4px 10px rgba(0, 0, 0, 0.2);
+
+    &&& {
+        margin-right: ${openButtonAvatarSizeNumber / 2}px;
+        margin-bottom: ${openButtonAvatarSizeNumber / 5}px;
+    }
 
     &:hover {
         background-color: #005b82;
